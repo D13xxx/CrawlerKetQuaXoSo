ac6dfbc63e61ec923a96f34a373a22397e0a1d74
diepnv <diepnv@hellomedia.vn>
2019-07-03 14:08:17 +0700

    update

---
+++ api/modules/v1/controllers/ResultController.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace api\modules\v1\controllers;
+
+use api\modules\v1\resources\Article;
+use yii\data\ActiveDataProvider;
+use yii\rest\ActiveController;
+use yii\rest\IndexAction;
+use yii\rest\OptionsAction;
+use yii\rest\CreateAction;
+use yii\rest\UpdateAction;
+use yii\rest\DeleteAction;
+use yii\rest\Serializer;
+use yii\rest\ViewAction;
+use yii\web\HttpException;
+use Yii;
+use common\models\Result;
+/**
+ * @SWG\Swagger(
+ *     schemes={"http","https"},
+ *     basePath="/",
+ *     @SWG\Info(
+ *         version="1.0.0",
+ *         title="Yii2-Starter-Kit API Documentation",
+ *         description="API description...",
+ *         termsOfService="",
+ *         @SWG\License(
+ *             name="BSD License",
+ *             url="https://raw.githubusercontent.com/yii2-starter-kit/yii2-starter-kit/master/LICENSE.md"
+ *         )
+ *     ),
+ * )
+ * @author Eugene Terentev <eugene@terentev.net>
+ */
+class ResultController extends ActiveController
+{
+    /**
+     * @var string
+     */
+    public $modelClass = 'api\modules\v1\resources\Article';
+
+    /**
+     * @SWG\Get(path="/v1/article/index",
+     *     tags={"article", "index"},
+     *     summary="Retrieves the collection of Articles.",
+     *     @SWG\Response(
+     *         response = 200,
+     *         description = "Article collection response",
+     *         @SWG\Schema(ref = "#/definitions/Article")
+     *     ),
+     * )
+     *
+     * @SWG\Get(path="/v1/article/view",
+     *     tags={"Article"},
+     *     summary="Displays data of one article only",
+     *     @SWG\Response(
+     *         response = 200,
+     *         description = "Used to fetch information of a specific article.",
+     *         @SWG\Schema(ref = "#/definitions/Article")
+     *     ),
+     * )
+     *
+     * @SWG\Options(path="/v1/article/options",
+     *     tags={"Article"},
+     *     summary="Displays the options for the article resource.",
+     *     @SWG\Response(
+     *         response = 200,
+     *         description = "Displays the options available for the Article resource",
+     *         @SWG\Schema(ref = "#/definitions/Article")
+     *     ),
+     * )
+     */
+    public function actions()
+    {
+        return [
+            'index' => [
+                'class' => IndexAction::class,
+                'modelClass' => $this->modelClass,
+                'prepareDataProvider' => [$this, 'prepareDataProvider']
+            ],
+            'view' => [
+                'class' => ViewAction::class,
+                'modelClass' => $this->modelClass,
+                'findModel' => [$this, 'findModel']
+            ],
+            'options' => [
+                'class' => OptionsAction::class,
+
+            ]
+        ];
+    }
+
+    /**
+     * @return ActiveDataProvider
+     */
+    public function prepareDataProvider()
+    {
+        return new ActiveDataProvider(array(
+            'query' => Article::find()->with('category', 'articleAttachments')->published()
+        ));
+    }
+
+    /**
+     * @param $id
+     * @return array|null|\yii\db\ActiveRecord
+     * @throws HttpException
+     */
+    public function actionTimKiemTheoNgay(){
+        $tuNgay = Yii::$app->request->post('tuNgay');
+        $denNgay = Yii::$app->request->post('denNgay');
+        if (!empty($denNgay) && !empty($tuNgay)){
+            $datas = Result::find()->andWhere(['>=', 'postdate',$tuNgay])->andWhere(['<', 'postdate',$denNgay]);
+        }else{
+            $datas = Result::find()->all();
+        }
+        $datasArray=[];
+        foreach ($datas as $data){
+            $datas = new \api\modules\v1\models\Result();
+            $datas->Id = $data->id;
+            $datas->PostDate = $data->postdate;
+            $datas->Result = $data->result;
+            $datas->Province = $data->province;
+            $datasArray[]=$datas;
+        }
+        return $this->asJson([
+            'status' => true,
+            'code' => 200,
+            'data'=>[
+                'datasArray' => $datasArray,
+            ]
+
+        ]);
+    }
+}
---
+++ api/modules/v1/models/Result.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace api\modules\v1\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "result".
+
+ */
+class Result
+{
+    public $Id;
+    public $PostDate;
+    public $Result;
+    public $Province;
+}
